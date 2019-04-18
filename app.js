5070ff43e56dfd2256535d9d33c99c6898ea6f65
Aishwarya Chaturvedi <chaturvedi.aishwarya@gmail.com>
2019-04-18 15:59:31 +0530

    Add viewing engine

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const bodyParser = require('body-parser')
+const handlebars  = require('express-handlebars')
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.engine('handlebars', handlebars({defaultLayout: 'index'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+    res.render('home');
+})
 
 app.listen(process.env.PORT, () => {
     console.log("server is listening on: ", process.env.PORT)
